4e5b8755ffb70d36eff1efbcd4ce230d473c5c15
NTSALA KANANELO <ntsalakananelo89@gmail.com>
2025-02-25 12:48:05 +0200

    Update pat1-subtask2.cpp

--- pat1-subtask2.cpp
+++ pat1-subtask2.cpp
@@ -3,29 +3,29 @@
 int main() {
   int temperature1, temperature2, temperature3;
 
-std:: count out << "Enter the first temperature reading: ";
-std:: count in >> temperature1;
+std:: cout << "Enter the first temperature reading: ";
+std:: cin >> temperature1;
 
-std:: count out << "Enter the second temperature reading: ";
-std:: coount in >> temperature2;
+std:: cout << "Enter the second temperature reading: ";
+std:: cin >> temperature2;
 
 // Check if there is an increase on the second reading by more than 50 degrees
 if (temperature2 - temperature1 >50 ) {
-count out <<"Reduce fryer heat before taking the third reading.\n "
-  return 0;
+cout<<"Reduce fryer heat before taking the third reading.\n ";
+
 }
-count out <<"Enter the third temperature reading:";
-count in >> temperature 3;
+cout <<"Enter the third temperature reading:";
+cin >> temperature 3;
 
 //Check if there is an increase between the second and the third readings is less than 10 degrees
 if (temperature3 - temperature 2 < 10){
-count out <<"Increase the fryer heat before taking the third reading./n";
-return 0;
+cout <<"Increase the fryer heat before taking the third reading./n";
+
 }
 
 //Check if the final temperature is within the acceptable range
 if (temperature 3>= 150 && temperature 3 <=190){
-count out  <<"Oil is not ready for frying!\n";
+cout  <<"Oil is not ready for frying!\n";
 }
 
 return 0;
