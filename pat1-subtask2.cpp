8df33ca2e1749285339aea6df1cf06f1586182fb
NTSALA KANANELO <ntsalakananelo89@gmail.com>
2025-02-25 09:27:04 +0200

    Update and rename README.md to pat1-subtask2.cpp

---
+++ pat1-subtask2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  int temperature1, temperature2, temperature3;
+
+std:: count out << "Enter the first temperature reading: ";
+std:: count in >> temperature1;
+
+std:: count out << "Enter the second temperature reading: ";
+std:: coount in >> temperature2;
+
+if(
+
